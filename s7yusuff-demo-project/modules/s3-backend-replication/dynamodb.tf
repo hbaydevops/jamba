5141e09119d4f171d2d40973eac017225b2e4406
devopseasylearning <info@devopseasylearning.com>
2025-02-06 22:25:23 -0500

    s

---
+++ s7yusuff-demo-project/modules/s3-backend-replication/dynamodb.tf
@@ -0,0 +1,17 @@
+resource "aws_dynamodb_table" "tf-state-lock" {
+  provider       = aws.source
+  name           = format("%s-%s-%s-tf-state-lock", var.common_tags["environment"], var.common_tags["owner"], var.common_tags["project"])
+  hash_key       = "LockID"
+  # read_capacity  = 20
+  # write_capacity = 20
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags = merge(var.common_tags,{
+  Name           = format("%s-%s-%s-tf-state-lock", var.common_tags["environment"], var.common_tags["owner"], var.common_tags["project"])
+  }
+  ) 
+}
+
